e63d90dca4f776300b0576dfe070db591ee08ad5
IvanDLar <ivandiaz.lara.m@gmail.com>
2022-05-12 10:13:40 -0500

    Adding python files

---
+++ Py/triangle_area.py
@@ -0,0 +1,12 @@
+"""
+Compute the triangle of an area, given base and height
+
+Gilberto Echeverria
+"""
+
+base = float(input("Enter the base: "))
+height = float(input("Enter the height: "))
+
+area = base * height / 2
+
+print(f"Triangle area is: {area}")
